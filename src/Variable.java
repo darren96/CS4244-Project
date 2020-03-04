56e0df01f7e1dc3f5f8aa465662418bacac532dd
darren96 <darren.oyk@gmail.com>
2020-03-04 10:50:35 +0800

    Add Variable.java

--- src/Variable.java
+++ src/Variable.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class Variable {
+    Integer variable;
+    Boolean assigned;
+    Integer noOfAppearances;
+
+    public Variable(Integer variable) {
+        this.variable = variable;
+        this.assigned = false;
+        this.noOfAppearances = 1;
+    }
 }
