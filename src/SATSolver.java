d4216f49cbc0f6a350e2df114a21a1f7a62a9b43
darren96 <darren.oyk@gmail.com>
2020-03-15 21:22:16 +0800

    Remove unused import

--- src/SATSolver.java
+++ src/SATSolver.java
@@ -1,9 +1,6 @@
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.io.*;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 public class SATSolver {
     static List<Variable> variables = new ArrayList<>();
